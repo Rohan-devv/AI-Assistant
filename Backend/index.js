0d5898f3b31385c6b6d0c73fda8011e3c7fde828
Rohan-devv <devv112004@gmail.com>
2025-07-23 23:43:12 +0530

    ndkmdk

--- Backend/index.js
+++ Backend/index.js
@@ -23,6 +23,7 @@ app.post("/ask", async (req, res) => {
     await local = promiseHooks()
     await local = promiseHooks()
     await local = promiseHooks()
+    await local = promiseHooks()
 
     const matchedProducts = products.filter(p => {
       const priceMatch = (() => {
